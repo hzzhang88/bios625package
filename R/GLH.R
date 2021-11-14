a94c98b54e61630942c605bdb58daec67773d46b
Han Zhang <hzzhang@umich.edu>
2021-11-14 17:22:55 -0500

    full function

--- R/GLH.R
+++ R/GLH.R
@@ -10,7 +10,9 @@
 #'
 #'@param intercept whether to include intercept in the model, the default is True
 #'
-#'@param c contrast matrix,
+#'@param predict predict matrix used to estimate the outcomes
+#'
+#'@param contrast contrast matrix,
 #'
 #'@param alpha significance level, the default value is 0.05
 #'
@@ -26,7 +28,9 @@
 
 
 library(Matrix)
-GLH <- function(x,y,intr = TRUE, c,rhs = 0 ,alpha = 0.05){
+GLH <- function(x,y,intr = TRUE, predict,contrast,rhs = 0 ,alpha = 0.05){
+  y = as.matrix(y)
+  row_name = names(x)
   n = length(y) ### the number of observations
   if (intr == F & length(x)==1) {  #convert a vector into a matrix
     x= as.matrix(x)
@@ -37,23 +41,81 @@ GLH <- function(x,y,intr = TRUE, c,rhs = 0 ,alpha = 0.05){
   if (intr == TRUE) { # add intercept column into matrix
     x = cbind(rep(1,n),x)
   }
-  rank = rankMatrix(c)[[1]]# calculate the rank of contrast matrix
   p = dim(x)[2] # the p-1 covariates
-  if (length(rhs) == 1){
-    rhs = as.matrix(rep(rhs,nrow(c)))
-  } else {
-    rhs = as.matrix(rhs)
-  }
+
+  ### coeffecient part ###
   XTX_inv = solve(t(x)%*%x)
   beta_hat = XTX_inv %*% t(x) %*% y
   res = x%*%beta_hat-y
-  sigma_hat = (t(res) %*%res)/(n-p)
-  numarator = t(c%*%beta_hat - rhs) %*% solve((c%*%XTX_inv%*%t(c))) %*%(c%*%beta_hat - rhs)
-  F_value = as.vector(numarator/rank/sigma_hat)
+  sigma_hat = as.vector((t(res) %*%res)/(n-p))
+  var_beta_hat = sigma_hat * XTX_inv
+  beta_var = diag(var_beta_hat)
+  beta_error = sqrt(beta_var)
+  t_statistic = beta_hat/beta_error
+  pt_statistic = 2*(1-pt(abs(t_statistic),df = (n-p)))
+  conint_low = beta_hat + qt(alpha/2,(n-p))*beta_error
+  conint_high = beta_hat - qt(alpha/2,(n-p))*beta_error
+  coeff = cbind(beta_hat,beta_error,t_statistic,pt_statistic,conint_low,conint_high)
+  colnames(coeff) = c("Estimate","Std. Error","t value","Pr(>|t|)","Confident interval:Low","Confident interval:High" )
+  if (intr == T) {
+    rownames(coeff) = c("Intercept",paste0("beta",1:(p-1)))
+  } else {
+    rownames(coeff) = c(paste0("beta",1:p))
+  }
+  ###R squared and adjusted R squared ###
+  y_var = var(y)[1,1]
+  R_squared = 1-sigma_hat*(n-p)/(n-1)/y_var
+  R_squared_adjusted = 1-sigma_hat/y_var
+  R_list= list("Multiple R-squared"=R_squared,"Adjusted R-squared"=R_squared_adjusted)
 
-  p_statistic = pf(F_value, df1=rank,df2=(n-p),lower.tail = F)
+  ###prediction function ####
+  if (!is.null(predict)) {
+    if (!is.matrix(predict)){
+      warning("Automatedly covert the predict into a matrix, please check whether the result is expected")
+      predict = as.matrix(predict)
+    }
+    if (ncol(predict) == (p-1)){
+      warning("Automatedly append 1s into the matrix, please check whether the result is expected")
+      predict=cbind(1,predict)
+    } else if (ncol(predict) != p & ncol(predict) != p-1) {
+      stop("The predict matrix does not match the correct dimmensions")
+    }
+    predicted_value = predict %*% beta_hat
+    var_predicted_value = sigma_hat %*% predict %*% XTX_inv %*% t(predict)
+    predicted_value_error = sqrt(diag(var_predicted_value))
+    predicted_value_confint_low = predicted_value +  qt(alpha/2,(n-p))*predicted_value_error
+    predicted_value_confint_high = predicted_value -  qt(alpha/2,(n-p))*predicted_value_error
+    predicted_result = cbind(predicted_value,predicted_value_error,predicted_value_confint_low,predicted_value_confint_high)
+  } else {
+    message("Didn't use prediction function")
+  }
 
-  return(list(F_statistic = F_value, p_value = p_statistic))
+  ### General linear hypothesis###
+  if (!is.null(contrast)){
+    if (!is.matrix(contrast)) {
+      warning("Automatedly covert the predict into a 1 row matrix, please check whether the result is expected")
+      contrast = matrix(contrast,byrow = T,nrow = 1)
+    }
+    if (nol(contrast) == p-1) {
+      warning("Automatedly append 0s into the matrix, please check whether the result is expected")
+      contrast = cbind(0,contrast)
+    } else if (ncol(contrast) != p & ncol(predict) != p-1) {
+      stop("The contrast matrix does not match the correct dimmensions")
+    }
+    rank = rankMatrix(contrast)[[1]]# calculate the rank of contrast matrix
+    if (length(rhs) == 1){
+      rhs = as.matrix(rep(rhs,nrow(contrast)))
+    } else {
+      rhs = as.matrix(rhs)
+    }
+    numerator = t(contrast%*%beta_hat - rhs) %*% solve((contrast%*%XTX_inv%*%t(contrast))) %*%(contrast%*%beta_hat - rhs)
+    F_value = as.vector(numerator/rank/sigma_hat)
+    pF_statistic = pf(F_value, df1=rank,df2=(n-p),lower.tail = F)
+  } else {
+    message("Didn't use the general linear hypothesis function")
+  }
+
+  return(list(F_statistic = F_value, p_value = pF_statistic))
 }
 
 
