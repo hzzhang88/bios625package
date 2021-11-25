2b155ce54ede5a6c137e1a3718278d2046830dc8
Han Zhang <hzzhang@umich.edu>
2021-11-25 10:36:13 -0500

    test

--- tests/testthat/test-GLH_test.R
+++ tests/testthat/test-GLH_test.R
@@ -4,6 +4,7 @@ test_that("multiplication works", {
   x3 = sample(20:80,10000, replace = T)
   y = rnorm(10000,mean = 0,sd=sqrt(10))
   data("mtcars")
+  library(car)
   # check if the coefficient are the same
   expect_equal(as.vector(lm(y~x1+x2+x3)$coefficients['(Intercept)']),
                as.vector(GLH(list(x1,x2,x3),y,intr = T)$coefficient['Intercept','Estimate']))
@@ -15,14 +16,27 @@ test_that("multiplication works", {
   expect_message(GLH(list(x1,x2,x3),y,intr = T)$coefficient['Intercept','Estimate'],"Didn't use prediction function")
   expect_message(GLH(list(x1,x2,x3),y,intr = T)$coefficient['Intercept','Estimate'],"Didn't use the general linear hypothesis function")
   #check if the p values are the same
-  expect_equal(as.vector(round(0.2493,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
-                                    contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs = 0.8)$Hypothesis[1,"p_value"]),4))
-  expect_equal(as.vector(round(0.2493,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
-                                                              contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs = matrix(c(0.8),1,1))$Hypothesis[1,"p_value"]),4))
+  expect_equal(as.vector(car::linearHypothesis(model =lm(mtcars$mpg~mtcars$cyl+mtcars$hp+mtcars$drat+mtcars$wt),hypothesis.matrix=matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs=0.8)[2,"Pr(>F)"]),
+               as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs = 0.8)$Hypothesis[1,"p_value"]))
 
-  expect_equal(as.vector(round(0.04809,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
-                                                              contrast = matrix(c(0,0,1,-1,0,0,0,0,1,-1),byrow = T,nrow =2),rhs = c(0.8,0.8))$Hypothesis[1,"p_value"]),4))
-  # check the warning messages
+  expect_equal(as.vector(car::linearHypothesis(model =lm(mtcars$mpg~mtcars$cyl+mtcars$hp+mtcars$drat+mtcars$wt),hypothesis.matrix=matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs=0.8)[2,"Pr(>F)"]),
+               as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs =matrix(c(0.8),1,1))$Hypothesis[1,"p_value"]))
+
+  expect_equal(as.vector(car::linearHypothesis(model =lm(mtcars$mpg~mtcars$cyl+mtcars$hp+mtcars$drat+mtcars$wt),hypothesis.matrix=matrix(c(0,0,1,-1,0,0,0,0,1,-1),byrow = T,nrow =2),rhs=c(0.8,0.8))[2,"Pr(>F)"]),
+               as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,contrast = matrix(c(0,0,1,-1,0,0,0,0,1,-1),byrow = T,nrow =2),rhs =c(0.8,0.8))$Hypothesis[1,"p_value"]))
+
+  # expect_equal(as.vector(round(0.2493,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
+  #                                                             contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs = matrix(c(0.8),1,1))$Hypothesis[1,"p_value"]),4))
+  #
+  #
+  # expect_equal(as.vector(round(0.2493,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
+  #                                   contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs = 0.8)$Hypothesis[1,"p_value"]),4))
+  # expect_equal(as.vector(round(0.2493,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
+  #                                                             contrast = matrix(c(0,0,1,-1,0),byrow = T,nrow =1),rhs = matrix(c(0.8),1,1))$Hypothesis[1,"p_value"]),4))
+  #
+  # expect_equal(as.vector(round(0.04809,4)),round(as.vector(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, intr = TRUE,
+  #                                                             contrast = matrix(c(0,0,1,-1,0,0,0,0,1,-1),byrow = T,nrow =2),rhs = c(0.8,0.8))$Hypothesis[1,"p_value"]),4))
+  # # check the warning messages
   expect_warning(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, predict = c(6,100,3.75,2.90),intr = T),
                "Automatically covert the predict into a matrix,please check whether the result is expected")
   expect_warning(GLH(x= list(mtcars$cyl,mtcars$hp,mtcars$drat,mtcars$wt), y = mtcars$mpg, predict = c(6,100,3.75,2.90),intr = T),
